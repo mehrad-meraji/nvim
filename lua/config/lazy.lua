3e3fdfdb722ce3b3b2f1b91dffc796916654a164
mmeraji <mmeraji@tpl.ca>
2025-01-18 10:01:14 -0500

    Update Nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,8 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.util.chezmoi" },
+    { import = "lazyvim.plugins.extras.util.dot" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignore = false,
+    },
+  },
+}
